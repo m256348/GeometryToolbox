767910532ce7a52fdef48c2da00aa125f00731fa
Michael Kutzer <kutzer@usna.edu>
2024-09-05 10:04:42 -0400

    Line functions

---
+++ GeometryToolboxFunctions/fitLine.m
@@ -0,0 +1,38 @@
+function varargout = fitLine(pnts)
+% FITLINE fits the equation of a 2-dimensional line to a set of points.
+%   abc = FITLINE(pnts) fit the equation of a 2-dimensional line 
+%   (c_1*x + c_2*y + c_{3) = 0) to a provided set of points. 
+%   Coefficients are calculated in the Hessian normal form using singular 
+%   value decomposition to determine the unit normal.
+%
+%   [..., meanError] = FITLINE(...) additionally returns the mean error 
+%   of the distance of provided points and projections.
+%
+%   Input(s)
+%       pnts - 2xM array containing points where M >= 2
+%
+%   Output(s)
+%       abc - 1x3 array containing coefficients for line equation
+%           Line (2D): [a,b,c] such that a*x + b*y + c = 0
+%       meanError - mean error of the distance of provided points and their
+%                   projections onto the line
+%
+%       NOTE: If the minimum number of points is provided, the line will
+%             be "oriented" based on the order of points provided using the 
+%             n-dimensional cross product (nCross) if the
+%             TransformationToolbox is installed.
+%
+%   See also proj2line fitPlane
+%
+%   M. Kutzer, 05Sep2024, USNA
+
+%% Check input(s)
+narginchk(1,1);
+
+[nD,mPnts] = size(pnts);
+if nD ~= 2
+    error('Points must be specified in 2-dimensions.');
+end
+
+%% Fit line
+varargout = fitPlane(pnts);
---
+++ GeometryToolboxFunctions/proj2line.m
@@ -0,0 +1,35 @@
+function varargout = proj2line(abc,X)
+% PROJ2LINE projects a set of points to a specified line
+%   Xproj = PROJ2LINE(pln,X) projects the M points contained in the NxM
+%   array X to the line specified as either a 1x(N+1) array or a 
+%   planeModel object. 
+%
+%       abc - 1x3 array containing coefficients for line equation
+%           Line (2D): [a,b,c] such that a*x + b*y + c = 0
+%       X   - 2xM array containing points where M >= N
+%
+%   [..., meanError] = PROJ2LINE(...) additionally returns the mean 
+%   error of the Euclidean distance between the provided points and 
+%   their corresponding projections.
+%
+%   [..., Error] = PROJ2LINE(...) additionally returns errors for each
+%   projected point.
+%
+%   See also fitLine, proj2plane
+%
+%   M. Kutzer, 05Sep2024, USNA
+
+%% Check input(s)
+narginchk(2,2);
+
+[nD,mPnts] = size(pnts);
+if nD ~= 2
+    error('Points must be specified in 2-dimensions.');
+end
+
+if numel(abc) ~= 3
+    error('Line must be defined as a 3-element array.');
+end
+
+%% Project to line
+varargout = proj2plane(abc,X);
