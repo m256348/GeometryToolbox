b535b4be4fcb79607a549d1dce924881b47fb6d7
Michael Kutzer <kutzer@usna.edu>
2024-10-10 17:55:33 -0400

    Update intersectPlaneSegment.m

--- GeometryToolboxFunctions/intersectPlaneSegment.m
+++ GeometryToolboxFunctions/intersectPlaneSegment.m
@@ -1,4 +1,4 @@
-function pnt = intersectPlaneSegment(abcd,pnts)
+function pnt = intersectPlaneSegment(abcd,pnts,ZERO)
 % INTERSECTPLANSEGMENT finds the point of intersection, if it exists,
 % between a plane and a segment.
 %   pnt = INTERSECTPLANSEGMENT(abcd,pnts)
@@ -6,6 +6,13 @@ function pnt = intersectPlaneSegment(abcd,pnts)
 %   Inputs:
 %       abcd - 1x4 array containing the coefficients of a plane
 %       pnts - 3x2 array containing two 3D end-points of a segment
+%       ZERO - [OPTIONAL] positive scalar value close to zero. Default
+%              value is 1e-8.
+%
+%   Output(s)
+%       pnt - 3x1 if a single point of intersection exists
+%       pnt - 3x2 if segment is contained in the plane
+%       pnt - empty set if no intersection exists
 %
 %   References:
 %       [1] https://mathworld.wolfram.com/Line-PlaneIntersection.html
@@ -13,9 +20,8 @@ function pnt = intersectPlaneSegment(abcd,pnts)
 %   M. Kutzer, 12Jun2020, USNA
 
 % Updates:
-%   27May2021 - Finished function
-
-% TODO - adjust function to eliminate warning, potentially add two outputs
+%   27May2021 - Updated function
+%   10Oct2024 - Completed function
 
 %% Check inputs
 narginchk(2,2);
@@ -25,10 +31,24 @@ if numel(abcd) ~= 4
 end
 abcd = reshape(abcd,1,[]);
 
-% TODO - check inputs
+if size(pnts,1) ~= 3 || size(pnts,2) ~= 2
+    error('Points in 3D must be defined as a 3x2 array');
+end
+
+%% Set default output
+pnt = [];
 
 %% Fit parametric line where s \in [0,1] defines the segment
 M = pnts*[0, 1; 1, 1]^(-1);
+
+%% Check for line parallel with plane
+if ads( dot(M(:,1),abcd(1:3).') ) < ZERO
+    % Segment and plane are parallel
+    if any( abs( abcd*[pnts; 1,1] ) < ZERO )
+        pnt = pnts;
+    end
+    return
+end
 
 %% Solve for s
 % M = [m1,m2];
@@ -41,8 +61,5 @@ d = abcd(4);
 s = (-d - abc*m2)/(abc*m1);
 
 if s >= 0 && s <= 1
-    pnt = M*s;
-else
-    warning('Intersection is outside the segment.');
-    pnt = M*s;
+    pnt = M*[s; 1];
 end
